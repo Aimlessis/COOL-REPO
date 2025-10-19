5567bace08e41f19ad8bdcbc3adc9e675305b7df
SHREK <ism.dlsr18@gmail.com>
2025-10-19 16:24:27 -0400

    positivos negativos y cero

---
+++ IFELIF/posnegnull.py
@@ -0,0 +1,9 @@
+num = float(input("escribe un numero para indicar si es positivo, negativo o cero: \n"))
+
+if num == 0:
+    print("es cero")
+
+if num > 0:
+    print("es positivo")
+else:
+    print("es negativo")
